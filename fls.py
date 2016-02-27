6299a7fc1d8c7f5f9458ed13ca5800bd6ed9b593
Stephan Hügel <urschrei@gmail.com>
2016-02-27 20:00:49 +0000

    Add index, 404, and 500 pages

--- fls.py
+++ fls.py
@@ -1,6 +1,14 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request, render_template, jsonify
+import json
 app = Flask(__name__)
 from hexagram import Hexagram, Trigram
+
+def request_wants_json():
+    best = request.accept_mimetypes \
+        .best_match(['application/json', 'text/html'])
+    return best == 'application/json' and \
+        request.accept_mimetypes[best] > \
+        request.accept_mimetypes['text/html']
 
 @app.route('/hexagram/<hexagram>.png')
 def hex_out(hexagram):
@@ -11,6 +19,30 @@ def hex_out(hexagram):
 def tri_out(trigram):
     generated = Trigram([int(elem) for elem in trigram])
     return send_file(generated.dump_image(), mimetype='image/png')
+
+@app.route('/')
+def index():
+    return render_template('index.html'), 200
+
+@app.errorhandler(404)
+def page_not_found(error):
+    error = {
+        "Hexagram": "/hexagram/nnnnnn.png, where n is 1 (solid bar) or 0 (broken bar). Hexagrams are built bottom to top.",
+        "Trigram": "/trigram/nnn.png, where n is 1 (solid bar) or 0 (broken bar). Trigrams are built bottom to top."
+    }
+    if request_wants_json():
+        return jsonify(items=[json.dumps(error)])
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def app_error(error):
+    error = {
+        "Hexagram: /hexagram/nnnnnn.png, where n is 1 (solid bar) or 0 (broken bar). Hexagrams are built bottom to top.",
+        "Trigram: /trigram/nnn.png, where n is 1 (solid bar) or 0 (broken bar). Trigrams are built bottom to top."
+    }
+    if request_wants_json():
+        return jsonify(items=[json.dumps(error)])
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run()
